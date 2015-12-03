6d2917dc788d8793b575e1525409dee807b19408
Luavis <luaviskang@gmail.com>
2015-12-04 04:31:47 +0900

    Testing download and parse LRC

--- Kasa.swiftTests/Kasa_swiftTests.swift
+++ Kasa.swiftTests/Kasa_swiftTests.swift
@@ -23,11 +23,33 @@ class Kasa_swiftTests: XCTestCase {
         super.tearDown()
     }
 
+    func testLyricParsing() {
+        let lyrics = Lyrics.decode("[00:12.39]마주 보고 서 있는대도<br>[00:17.15]마음까진 보이지 않아<br>[00:22.32]혼자 있는 빈 시간마다<br>")!
+        XCTAssert(lyrics[13]?.lyric == "마주 보고 서 있는대도")
+        XCTAssert(lyrics[15]?.lyric == "마주 보고 서 있는대도")
+        XCTAssert(lyrics[18]?.lyric == "마음까진 보이지 않아")
+        XCTAssert(lyrics[23]?.lyric == "혼자 있는 빈 시간마다")
+    }
+
     func testDownloadLyric() {
         let manager = LyricManager.manager
-        let (suc, msg) = manager.id3Md5Hash("/Users/Luavis/Music/iTunes/iTunes Media/Music/Various Artists/프로듀사 (KBS 2TV 금토드라마) OST/1-05 우리 둘.mp3")
-        print(msg)
-        XCTAssert(suc, "Pass")
+        let testBundle = NSBundle(forClass: self.dynamicType)
+        let filePath = testBundle.pathForResource("test", ofType: "mp3")!
+
+        let readyExpectation = expectationWithDescription("ready for download")
+
+        manager.getLyric(filePath) { lyrics in
+            XCTAssert(lyrics[13]?.lyric == "마주 보고 서 있는대도")
+            XCTAssert(lyrics[15]?.lyric == "마주 보고 서 있는대도")
+            XCTAssert(lyrics[18]?.lyric == "마음까진 보이지 않아")
+            XCTAssert(lyrics[23]?.lyric == "혼자 있는 빈 시간마다")
+
+            readyExpectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(20.0) { error in
+            XCTAssertNil(error, "Timeout error")
+        }
     }
     
     func testPerformanceExample() {
